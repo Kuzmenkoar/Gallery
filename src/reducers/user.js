cb64d5d423df5e7beb384bf75d7f35e1d108d5b8
kuzmenkoar <kuzmenko.vyshneve@gmail.com>
2017-02-10 15:55:12 +0200

    Start

---
+++ src/reducers/user.js
@@ -0,0 +1,9 @@
+/**
+ * Created by kuzmenkoar on 05.02.17.
+ */
+const	initialState	=	{
+    name:	'Аноним'
+}
+export	default	function	user(state	=	initialState)	{
+    return	state
+}
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+var	path	=	require('path');
+var	webpack	=	require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports	=	{
+    devtool:	'cheap-module-eval-source-map',
+    entry:	[
+        'webpack-hot-middleware/client',
+        'babel-polyfill',
+        './src/index'
+    ],
+    output:	{
+        path:	path.join(__dirname,	'dist'),
+        filename:	'bundle.js',
+        publicPath:	'/static/'
+    },
+    module:	{	//Обновлено
+        preLoaders:	[	//добавили	ESlint	в	preloaders
+            {
+                test:	/\.js$/,
+                loaders:	['eslint'],
+                include:	[
+                    path.resolve(__dirname,	"src"),
+                ],
+            }
+        ],
+        loaders:	[	//добавили	babel-loader
+            {
+                loaders:	['react-hot', 'babel-loader'],
+                include:	[
+                    path.resolve(__dirname,	"src"),
+                ],
+                test:	/\.js$/,
+                plugins:	['transform-runtime'],
+            },
+            {
+                test: /\.scss$/,
+                loader: 'style!css!sass'
+            }
+        ]
+    },
+    plugins:	[
+        new	webpack.optimize.OccurenceOrderPlugin(),
+        new	webpack.HotModuleReplacementPlugin(),
+        new	webpack.NoErrorsPlugin()
+    ]
+};
